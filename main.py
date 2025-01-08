26d9c6a310296b03e35d5096a56afc3bdc85904a
Apollo59 <72157015+Apollo59@users.noreply.github.com>
2025-01-08 13:49:16 -0500

    I HATE OCR

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from bs4 import BeautifulSoup
 import cv2
 import pyautogui
-import easyocr
+#import easyocr
 image = pyautogui.screenshot("test.png")
 
 imagepath = 'test.png'
 
 read = cv2.imread(imagepath)
 
-reader = easyocr.Reader(['en'], gpu=False)
+#reader = easyocr.Reader(['en'], gpu=False)
 
-text = reader.readtext('test.png', detail = 0)
+#text = reader.readtext('test.png', detail = 0)
 
 file = open("test.txt", "w")
 file.write(text)
